b9b0d786d2e8d5f202212f8705ca5abf23c653ac
Christian Bundy <christianbundy@fraction.io>
2018-12-18 17:38:53 -0800

    Get Scuttlebutt messages and blobs working

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa');
+const debug = require('debug')('localhost')
 const fs = require('fs')
 const path = require('path')
-const debug = require('debug')('localhost')
+
+const getSsbMsg = require('./lib/get-ssb-msg')
+const getSsbBlob = require('./lib/get-ssb-blob')
 
 debug.enabled = true
 
@@ -11,15 +14,14 @@ const port = 8000
 const indexHtml = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
 
 const schemes = {
-  ssb: (uri) => {
-    return `in the future this should show ${uri} from Scuttlebutt`
-  },
-  dat: (uri) => {
+  'ssb:blob': getSsbBlob,
+  'ssb:message': getSsbMsg,
+  dat: async (uri) => {
     return `in the future this should show ${uri} from Dat`
   }
 }
 
-app.use(async ctx => {
+app.use(async (ctx, next) => {
   const s = ctx.request.query.s
 
   if (s && s.startsWith('web+')) {
@@ -27,20 +29,24 @@ app.use(async ctx => {
     const redirect ='/' + s.replace('web+', '')
     debug('redirecting to %s', redirect)
     ctx.status = 302;
-    return ctx.redirect(redirect);
+    ctx.redirect(redirect);
+    return next()
   } else if (ctx.request.path === '/') {
-    debug('home')
-    return ctx.body = indexHtml;
+    debug('homepage requested')
+    ctx.response.body = indexHtml
+    return next()
   } else {
-    debug('else')
+    debug('something else requested')
     const uri = ctx.request.path.replace('/', '')
 
     let handler
 
     const handled = Object.keys(schemes).some(scheme => {
       const prefix = `${scheme}:`
+      debug(prefix)
       const match = uri.startsWith(prefix)
       if (match) {
+        debug('found a match: %s', scheme)
         handler = schemes[scheme]
       }
 
@@ -48,10 +54,19 @@ app.use(async ctx => {
     })
 
     if (handled) {
-      return ctx.body = handler(uri)
+      debug('handling now')
+      const data = await handler(uri)
+
+      if (typeof data !== 'string') {
+        ctx.response.set('Content-Disposition', `inline; filename=${uri}`)
+      }
+      ctx.response.body = data
+      return next()
     } else {
+      debug('redirecting now')
       ctx.status = 301;
-      return ctx.redirect('/');
+      ctx.redirect('/');
+      return next()
     }
   }
 });
